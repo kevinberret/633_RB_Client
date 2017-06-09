b75ad1d40813dbc872d62de7d370c2ce8b810afc
student <student@DESKTOP-K784I9T>
2017-06-09 16:25:56 +0200

    MAJ du client, suppression des fichiers inutiles

--- src/application/ClientController.java
+++ src/application/ClientController.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.io.IOException;
-import java.net.NetworkInterface;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
--- src/application/ClientMain.java
+++ src/application/ClientMain.java
@@ -1,6 +1,6 @@
 package application;
 
-public class Main {
+public class ClientMain {
 	public static void main(String[] args) {
 		ClientModel cm = new ClientModel();
 		ClientController cc = new ClientController(cm);
