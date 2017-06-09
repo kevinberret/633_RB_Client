d71c14df152f7d7f4414b01063410051b6b087b8
Kevin <Kevin@BERK-PC.home>
2017-06-09 20:09:54 +0200

    (suite commit précédent)

---
+++ src/application/Client.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Client {
+	private String uuid;
+	private String clientIp;
+	private ArrayList<String> files;
+	
+	public Client(String uuid, String clientIp, ArrayList<String> files) {
+		this.uuid = uuid;
+		this.clientIp = clientIp;
+		this.files = files;
+	}
+	
+	public Client(String uuid, String clientIp) {
+		this.uuid = uuid;
+		this.clientIp = clientIp;
+		this.files = null;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getClientIp() {
+		return clientIp;
+	}
+
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+
+	public ArrayList<String> getFiles() {
+		return files;
+	}
+
+	public void setFiles(ArrayList<String> files) {
+		this.files = files;
+	}
+}
